30f79e938db149a4d205a965edb5b56b37140683
makenamecham <makenamecham@mail.weber.edu>
2024-11-04 09:12:42 -0700

    11/4 KNN

--- Module 7/KNN_analysis.py
+++ Module 7/KNN_analysis.py
@@ -3,15 +3,26 @@
 
 import pandas as pd
 import numpy as np
-import get_data as gt # your package
+import get_data as gt  # your package
 
 # Constants
-K = 10 # Number of closest matches
-BASE_CASE_ID = 88763 #IMBD_id for 'Back to the Future'
+K = 10  # Number of closest matches
+BASE_CASE_ID = 88763  # IMBD_id for 'Back to the Future'
 
-def knn_analysis_driver(df, base_case, comparison_type, metric_stub, sorted_value='metric'):
-    # Create df of filter data
-    df[sorted_value] = df[comparison_type].map(lambda x: metric_stub(base_case[comparison_type], x))
+
+def metric_stub(base_case_value, comparator_value):
+    return 0
+
+
+def knn_analysis_driver(data_df, base_case, comparison_type, metric_func, sorted_value='metric'):
+    df = data_df.copy()  # Make a copy of the data frame
+    # WIP: Create df of filter data
+    df[sorted_value] = df[comparison_type].map(
+        lambda x: metric_func(base_case[comparison_type], x))
+    # Sort return values from function stub
+    sorted_df = df.sort_value(by=sorted_value)
+    sorted_df.drop(BASE_CASE_ID, inplace=True)  # Drop case base
+    print(sorted_df['title'].head(K))  # print first 10 values
 
 
 def main():
@@ -25,11 +36,15 @@ def main():
     data_file = f'{gt.DATA_FOLDER}/{dataset_file}'
     data = gt.load_data(data_file)
     print(f'Loaded {len(data)} records')
-    print(f'Data set Columns {data.columns})
+    print(f'Data set Columns {data.columns}')
     print(f'Data set Description {data.describe}')
 
-    # TODO: The rest of your code goes here
-
+    # Task 3: KNN Analysis Driver
+    print(f'Task 3: KNN Simple Analysis')
+    base_case = data.loc[BASE_CASE_ID]
+    print(f"Comparing all movies to our case: {base_case['title']}")
+    knn_analysis_driver(data_df=data, base_case=base_case, comparison_type='genres',
+                        metric_func=metric_stub, sorted_value='metric')
 
 
 if __name__ == '__main__':
